17b9b329a545bb8fc87e7c54c391819e95066af6
jiashutang <jst8712>
2019-10-23 16:46:28 +0800

    样式调整

--- src/components/application/Batch.vue
+++ src/components/application/Batch.vue
@@ -312,11 +312,11 @@ export default {
           value: '0',
           label: '待审核'
         }, {
-          value: '2',
-          label: '已退回'
-        }, {
           value: '3',
           label: '已审核'
+        }, {
+          value: '2',
+          label: '已退回'
         }
       ],
       startEndDate: [],
